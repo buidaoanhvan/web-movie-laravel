79ec97190b02295212b2dc60e947f5f6618d8ee3
buidaoanhvan <46972101+buidaoanhvan@users.noreply.github.com>
2020-10-14 19:32:16 +0700

    sua from add movie

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -3,51 +3,54 @@
     <div class="card"
         style="border-radius: 10px; overflow:hidden; -webkit-box-shadow: 0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0,0,0,0); box-shadow: 0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0,0,0,0); margin:20px">
         <div class="card-body">
-            <form action="/admin/addmovie" method="POST">
-                {{ csrf_field() }}
-                <h4 style="margin:10px; display: flex; justify-content:center">THÊM PHIM MỚI</h4>
-                <div class="form-group">
-                    <label>Tên Phim:</label>
-                    <input name="tenphim" type="text" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label>Link Hình:</label>
-                    <input name="linkhinh" type="text" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label>Link Phim:</label>
-                    <input name="linkphim" type="text" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="sel1">Thể Loại Phim:</label>
-                    <select name="theloai" class="form-control" id="sel1">
-                        <option value="Hành Động">Hành Động</option>
-                        <option value="Kinh Dị">Kinh Dị</option>
-                        <option value="Hài Hước">Hài Hước</option>
-                        <option value="Tình Cảm">Tình Cảm</option>
-                        <option value="Hoạt Hình">Hoạt Hình</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label>Ngày Ra Mắt:</label>
-                    <input name="ngaysanxuat" type="text" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label>Đánh Giá:</label>
-                    <input name="danhgia" type="text" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label>Diễn Viên:</label>
-                    <input name="dienvien" type="text" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="comment">Mô Tả:</label>
-                    <textarea name="mota" class="form-control" rows="5" id="comment"></textarea>
-                </div>
-                <div class="form-group" style=" margin:50px; display: flex; justify-content:center">
-                    <button style="width:20%;" type="submit" class="btn btn-success">Thêm Phim Mới</button>
-                </div>
-            </form>
+            <button id="1" class="btn btn-primary" style="margin:10px; display: flex; justify-content:center">THÊM PHIM
+                MỚI</button>
+            <div id="2" style="display:none">
+                <form action="/admin/addmovie" method="POST" style="margin-top:40px">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label>Tên Phim:</label>
+                        <input name="tenphim" type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Link Hình:</label>
+                        <input name="linkhinh" type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Link Phim:</label>
+                        <input name="linkphim" type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="sel1">Thể Loại Phim:</label>
+                        <select name="theloai" class="form-control" id="sel1">
+                            <option value="Hành Động">Hành Động</option>
+                            <option value="Kinh Dị">Kinh Dị</option>
+                            <option value="Hài Hước">Hài Hước</option>
+                            <option value="Tình Cảm">Tình Cảm</option>
+                            <option value="Hoạt Hình">Hoạt Hình</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label>Ngày Ra Mắt:</label>
+                        <input name="ngaysanxuat" type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Đánh Giá:</label>
+                        <input name="danhgia" type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Diễn Viên:</label>
+                        <input name="dienvien" type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="comment">Mô Tả:</label>
+                        <textarea name="mota" class="form-control" rows="5" id="comment"></textarea>
+                    </div>
+                    <div class="form-group" style=" margin:50px; display: flex; justify-content:center">
+                        <button style="width:20%;" type="submit" class="btn btn-success">Thêm Phim Mới</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
     <div class="row" style="margin:20px">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,7 +58,7 @@
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
-                                                                                                         document.getElementById('logout-form').submit();">
+                                                                                                                                                         document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
@@ -76,5 +76,14 @@
         </main>
     </div>
 </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $("#1").click(function() {
+            $("#2").toggle();
+        });
+    });
+
+</script>
 
 </html>
